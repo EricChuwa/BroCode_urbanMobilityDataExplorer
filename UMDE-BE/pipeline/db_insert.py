ab9c70c3c4bd6d5fbd8b6602b0bb4eb883040861
Albert-Afiti <a.afiti@alustudent.com>
2026-06-21 20:53:26 +0200

    Change: finalized paths & set batch size for db insertion

--- UMDE-BE/pipeline/db_insert.py
+++ UMDE-BE/pipeline/db_insert.py
@@ -4,6 +4,11 @@ import psycopg2.extras
 import os
 from dotenv import load_dotenv
 
-# Load Environment Variables
+# Paths
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+LOOKUP_PATH   = os.path.join(BASE_DIR, 'data', 'raw', 'taxi_zone_lookup.csv')
+ENRICHED_PATH = os.path.join(BASE_DIR, 'data', 'processed', 'enriched_trips.csv')
+
+# Load ENV
 load_dotenv(os.path.join(BASE_DIR, 'UMDE-BE', '.env'))
+BATCH_SIZE = 50_000
