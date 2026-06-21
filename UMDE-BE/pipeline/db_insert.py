e4d96a0d954af5a4ebe7e681bf585a932542bfcf
Albert-Afiti <a.afiti@alustudent.com>
2026-06-21 20:54:40 +0200

    db connection

--- UMDE-BE/pipeline/db_insert.py
+++ UMDE-BE/pipeline/db_insert.py
@@ -12,3 +12,13 @@ ENRICHED_PATH = os.path.join(BASE_DIR, 'data', 'processed', 'enriched_trips.csv'
 # Load ENV
 load_dotenv(os.path.join(BASE_DIR, 'UMDE-BE', '.env'))
 BATCH_SIZE = 50_000
+
+#  Connect 
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        dbname=os.getenv('DB_NAME')
+    )
